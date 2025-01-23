4d5cfa4f894ad6a5171c7858933c3dff15ccdc86
Chu_bingzhi <1054086938@qq.com>
2025-01-23 23:57:23 +0800

    pure virtual func

---
+++ C_plus/pure_virtual_func.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class A{
+	public:
+    virtual void f()=0;//纯虚函数
+    void g(){this->f();}//ok
+	A(){
+        //£();// 错误
+    }
+};
+class B:public A{
+    public :
+    void f(){cout<<"B:f()"<<endl;}
+};
+int main(){
+    B b;
+    b.g();
+    return 0;
+}
