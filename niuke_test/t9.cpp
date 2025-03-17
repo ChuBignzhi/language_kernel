63aa2e38ba1ac98236ac3e02428e3acd086d444f
Chu_bingzhi <1054086938@qq.com>
2025-03-18 07:47:33 +0800

    test

--- niuke_test/t9.cpp
+++ niuke_test/t9.cpp
@@ -1,51 +1,63 @@
-// #include <iostream>
-// using namespace std;
-
-// int fun(int x){
-//     int countx = 0;
-//     while (x) {
-//         countx++;
-//         x = x & (x - 1);
-//     }
-//     return countx;
-// }
-
-// int func(int x) {
-//     int count=0;
-//     while( x != 0xFFFFFFFF) {
-//         count++;
-//         x = x|(x+1);
-//     }
-//     return count;
-// }
-
-// int main() {
-//     // const int i = 0; 
-//     // int *j = (int *) &i; 
-//     // *j = 1; 
-//     // printf("%d,%d", i, *j);
-//     cout << fun(500) << endl;
-//     cout << func(500) << endl;
-//     return 0;
-// }
-
-#include<stdio.h>
-#include<iostream>
-#include<iomanip>
-
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){ 
-    // int sum; 
-    // for(int i=0; i<6; i+=2){ 
-    //     sum=i; 
-    //     for(int j = i; j<6; j++){
-    //         if(i+j>5) continue;
-    //         sum+=j;
-    //     } 
-    // }    
-    // printf("%d",sum);
-    // return 0; 
-    unsigned int uiA;
-    uiA = (0xAB & 0xCD) ^ (0xEF | 0xAB);
-    cout << hex << uiA << endl;
-} 
+
+// 归并排序一定要另外的数组去容纳之前的元素
+
+long long merge(vector<int>& nums, vector<int>& tmp, int left, int mid, int right) {
+    int i=left, j=mid+1, k=left;
+    long long count = 0;
+    while (i<=mid && j<=right) {
+        if(nums[i] <= nums[j]) {
+            tmp[k++] = nums[i++];
+        } else {
+            tmp[k++] = nums[j++];
+            count += mid-i+1;
+        }
+    }
+    
+    while(i<=mid) {
+        tmp[k++] = nums[i++];
+    }
+    while (j<=right) {
+        tmp[k++] = nums[j++];
+    }
+    
+    for(int p=left; p<=right; p++) nums[p] = tmp[p];
+
+    return count;
+}
+
+int mergeSort(vector<int>& nums, vector<int>& tmp, int left, int right) {
+    if(left>=right) return 0;
+
+    int mid = left + (right-left)/2;
+    long long count = 0;
+
+    count += mergeSort(nums, tmp, left, mid);
+    count += mergeSort(nums, tmp, mid+1, right);
+
+    count += merge(nums, tmp, left, mid, right);
+
+    return count;
+}
+
+
+long long InversePairs(vector<int>& nums) {
+    int len = nums.size();
+    if(len==0)  return 0;
+    vector<int> tmp(len);
+    return mergeSort(nums, tmp, 0, len-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for(int i=0; i<n; i++) {
+        cin >> vec[i];
+    }
+
+    cout << InversePairs(vec) << endl;
+    return 0;
+}
